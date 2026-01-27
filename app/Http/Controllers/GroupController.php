a902edaa69059cc5137b144bcf1113bc5465c371
bouqrt <bouchraelqrt@gmail.com>
2026-01-27 15:25:46 +0100

    Refactor GroupController for code consistency and readability

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,71 +7,56 @@ use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $groups=Group::all();
-        return view('groups.index',compact('groups'));}
+        $groups = Group::all();
+        return view('groups.index', compact('groups'));
+    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('groups.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
-            'name'=>'required|min3'        
+            'name' => 'required'
         ]);
-        group::create($request->all());
+
+        Group::create($request->all());
+
         return redirect()->route('groups.index')
-        ->with('success','Group created successfully.');
+            ->with('success', 'Group created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Group $group)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Group $group)
     {
-        return view('groups.edit',compact('group'));
+        return view('groups.edit', compact('group'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Group $group)
     {
         $request->validate([
-            'name'=>'required|min3'        
+            'name' => 'required|min:3'
         ]);
+
         $group->update($request->all());
+
         return redirect()->route('groups.index')
-        ->with('success','Group updated successfully.');
+            ->with('success', 'Group updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Group $group)
     {
         $group->delete();
+
         return redirect()->route('groups.index')
-        ->with('success','Group deleted successfully.');
+            ->with('success', 'Group deleted successfully.');
     }
 }
