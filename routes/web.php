56da72cf1bc66089f4f56e87502e78d45952d791
bouqrt <bouchraelqrt@gmail.com>
2026-01-27 16:36:36 +0100

    Add Contact model, controller, and migration for contacts resource

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 use App\Http\Controllers\GroupController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
 
 
 Route::resource('groups', GroupController::class);
+Route::resource('contacts', ContactController::class);
 Route::get('/', function () {
     return view('welcome');
 });
